b5e8d8ff4a6a63eedd48b540638d9d4adab7d064
stroodlepup <stroodlepup.extra@gmail.com>
2015-05-28 17:19:41 +0800

    Added Configuration options for use with heroku test database.

--- controllers/application.py
+++ controllers/application.py
@@ -12,8 +12,11 @@ DATABASE_NAME="project_daisy"
 MYSQL_STRING= 'mysql://'+ DATABASE_USERNAME +':'+DATABASE_PASSWORD+'@'+DATABASE_HOST+'/'+DATABASE_NAME
 POSTGRESQL_STRING= 'postgresql://'+DATABASE_USERNAME+':'+DATABASE_PASSWORD+'@'+DATABASE_HOST+'/'+DATABASE_NAME
 
+#database access for heroku
+HEROKU_STRING= 'postgresql://xyfmhyseuygdge:koKeu9q-v8x3O0W9GQHNxDsX5l@ec2-107-21-114-132.compute-1.amazonaws.com/d7fkne5j602151'
+
 app = Flask(__name__, static_folder='../assets', template_folder= '../views')
-app.config['SQLALCHEMY_DATABASE_URI'] = MYSQL_STRING
+app.config['SQLALCHEMY_DATABASE_URI'] = POSTGRESQL_STRING
 app.config['SQLALCHEMY_ECHO'] = True
 
 db=SQLAlchemy(app)
