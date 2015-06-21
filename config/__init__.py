5f8f799cfdf77dbf4e3f5aed48ee72177e31651c
stroodlepup <stroodlepup@gmail.com>
2015-06-21 12:45:24 +0800

    Re-prepared project for heroku deployment

--- config/__init__.py
+++ config/__init__.py
@@ -1,22 +1,7 @@
 __author__ = 'stroodlepup'
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
-# DEFAULT APPLICATION PROPERTIES
-APP_NAME="DAISY"
-DATABASE_USERNAME="stroodlepup"
-DATABASE_PASSWORD="arcreactor"
-DATABASE_HOST="localhost"
-DATABASE_NAME="project_daisy"
-
-
-# DATABASE STRINGS
-MYSQL_STRING= 'mysql://'+ DATABASE_USERNAME +':'+DATABASE_PASSWORD+'@'+DATABASE_HOST+'/'+DATABASE_NAME
-POSTGRESQL_STRING= 'postgresql://'+DATABASE_USERNAME+':'+DATABASE_PASSWORD+'@'+DATABASE_HOST+'/'+DATABASE_NAME
-
-# DATABASE ACCESS FOR HEROKU
-HEROKU_STRING= 'postgresql://xyfmhyseuygdge:koKeu9q-v8x3O0W9GQHNxDsX5l@ec2-107-21-114-132.compute-1.amazonaws.com/d7fkne5j602151'
-
+from config.config import APP_NAME, DATABASE_HOST, DATABASE_NAME, DATABASE_USERNAME, DATABASE_PASSWORD, MYSQL_STRING, POSTGRESQL_STRING, HEROKU_STRING
 # INSTANCES
 app = Flask(__name__)
 app.template_folder='../views'
